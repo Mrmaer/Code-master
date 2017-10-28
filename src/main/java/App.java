0960ceb196cec194ef7ddee8a9ee32ec5f79fadd
mrma <1007288450@qq.com>
2017-10-28 23:56:04 +0800

    优化

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,10 @@ import com.swing.ZouMian;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        ZouMian zouMian = new ZouMian();
-        zouMian.main();
+        ZouMian.main();
     }
 }
