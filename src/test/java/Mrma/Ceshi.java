431415a45267a6be64e6d7ddc3e74c9b632bfd87
mrma <1007288450@qq.com>
2017-10-28 23:55:46 +0800

    测试类

--- src/test/java/Mrma/Ceshi.java
+++ src/test/java/Mrma/Ceshi.java
@@ -1,15 +1,34 @@
 package Mrma;
 
+
 import com.read.Use;
+import com.sql.Shuju;
+import com.sqlsession.GetShu;
 import org.junit.Test;
-import org.springframework.beans.factory.BeanFactory;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Ceshi {
+
     @Test
     public void cehi() {
-        BeanFactory factory = new ClassPathXmlApplicationContext("Opera.xml");
+        ApplicationContext factory = new ClassPathXmlApplicationContext("peizhi.xml");
         Use use = (Use) factory.getBean("useOperating");
         use.read();
+    }
+
+    @Test
+    public void say(){
+        Say say = new Say("11");
+        System.out.println(say.toString());
+
+    }
+
+    @Test
+    public void textMybatisSpring(){
+        GetShu getShu = GetShu.getShu();
+        Shuju shuju = getShu.getshuju();
+        System.out.println(shuju.select("112"));
     }
 }
